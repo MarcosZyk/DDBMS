36782929717aa58fc5a6de68cb3f2e65942ab631
HeimingZ <zhuhmch@qq.com>
2022-06-04 18:20:33 +0800

    mongo network

--- mongodb/setup-configsvr.js
+++ mongodb/setup-configsvr.js
@@ -3,9 +3,9 @@ rs.initiate(
     _id: "configsvr_rs",
     configsvr: true,
     members: [
-      { _id: 0, host: "configsvr1:27019" },
-      { _id: 1, host: "configsvr2:27019" },
-      { _id: 2, host: "configsvr3:27019" }
+      { _id: 0, host: "172.20.0.3:27019" },
+      { _id: 1, host: "172.20.0.4:27019" },
+      { _id: 2, host: "172.20.0.5:27019" }
     ]
   }
 );
